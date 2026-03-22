51755523ece6112935d72d8ef1f0124bd01f2654
Abhinav Deval <abhinavkdeval08@gmail.com>
2026-03-22 12:09:17 +0530

    Update Code.c
    I've created snake body

--- Code.c
+++ Code.c
@@ -1,7 +1,33 @@
-//Snake Game
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
 
-int main(){
-  printf("Hello World");
+
+struct Node {
+    int x, y;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+void addNode(int x, int y) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->x = x;
+    newNode->y = y;
+    newNode->next = head;
+    head = newNode;
 }
+
+void removeTail() {
+    struct Node* temp = head;
+    if (!temp || !temp->next) return;
+
+    while (temp->next->next)
+        temp = temp->next;
+
+    free(temp->next);
+    temp->next = NULL;
+}
+
